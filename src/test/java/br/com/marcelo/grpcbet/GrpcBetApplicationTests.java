a11bb38368375f6f1e54370827e17b8356cefa5b
marcelowender <marcelowender@hotmail.com>
2021-07-27 23:55:18 -0300

    Initial commit

---
+++ src/test/java/br/com/marcelo/grpcbet/GrpcBetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcelo.grpcbet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcBetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
