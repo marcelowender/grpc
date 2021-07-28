8634b2d5357636f48ea7b3509d9afd977fe0415e
marcelowender <marcelowender@hotmail.com>
2021-07-28 02:07:47 -0300

    project modules

---
+++ bet-service/src/main/java/br/com/marcelo/grpcbet/GrpcBetApplication.java
@@ -0,0 +1,13 @@
+package br.com.marcelo.grpcbet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcBetApplication.class, args);
+	}
+
+}
